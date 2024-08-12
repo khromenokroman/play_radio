cdbbc4f5d9a441b75c9362fe71d3f1bb59d15baf
Хроменок Роман Владимирович <rkhromenok@t-argos.ru>
2024-08-12 22:59:21 +0200

    Не рабочий вариант

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -2,7 +2,7 @@
 
 radio::server::Server::Server(int port) : m_acceptor(
         std::make_unique<boost::asio::ip::tcp::acceptor>(m_io_context, boost::asio::ip::tcp::endpoint(
-                boost::asio::ip::tcp::v4(), port))) {
+                boost::asio::ip::tcp::v4(), port))), m_signals(m_io_context, SIGINT, SIGTERM) {
 
     ::fmt::print("Server started in port {}\n", port);
 
@@ -13,31 +13,58 @@ radio::server::Server::Server(int port) : m_acceptor(
         ::fmt::print(stderr, "Can not set name for thread aio context: {}", strerror(errno));
     }
 
+    m_signals.async_wait(
+            [this](boost::system::error_code ec, int signal_number) {
+                if (!ec) {
+                    switch (signal_number) {
+                        case SIGINT:
+                            ::fmt::print("Signal SIGINT {} -> stop()\n", signal_number);
+                            break;
+                        case SIGTERM:
+                            ::fmt::print("Signal SIGTERM {} -> stop()\n", signal_number);
+                            break;
+                        default:
+                            break;
+                    }
+                    stop();
+                } else {
+                    ::fmt::print(stderr, "Err: get signal SIGINT {}", ec.message());
+                }
+
+            });
     start_accepting();
 
 }
 
 radio::server::Server::~Server() {
-    if (m_asio_thread.joinable()) {
-        m_asio_thread.join();
-    }
+    m_io_context.stop();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void radio::server::Server::start_accepting() {
     std::shared_ptr<boost::asio::ip::tcp::socket> socket = std::make_shared<boost::asio::ip::tcp::socket>(m_io_context);
     auto callback = [&](boost::system::error_code const &error) {
         accept_handler(error, socket, m_buffer);
+
     };
     m_acceptor->async_accept(*socket, callback);
 }
 
+void radio::server::Server::run() {
+    if (m_asio_thread.joinable()) {
+        m_asio_thread.join();
+    }
+}
+
 void radio::server::Server::stop() {
     m_io_context.stop();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void radio::server::Server::accept_handler(const boost::system::error_code &error,
-                                           const std::shared_ptr<boost::asio::ip::tcp::socket>& socket,
+                                           const std::shared_ptr<boost::asio::ip::tcp::socket> &socket,
                                            boost::asio::streambuf &buffer) {
+    ::fmt::print("new connection\n");
     if (!error) {
         auto callback = [this, socket, &buffer](boost::system::error_code const &error, std::size_t bytes_transferred) {
             if (!error) {
@@ -45,6 +72,8 @@ void radio::server::Server::accept_handler(const boost::system::error_code &erro
                 std::string message;
                 std::getline(is, message);
                 ::fmt::print("Received from client: {}\n", message);
+                m_total_message++;
+                ::fmt::print("total: {}\n", m_total_message);
             } else {
                 if (error == boost::asio::error::eof) {
                     ::fmt::print("Connection closed by client\n");
